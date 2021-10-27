1c65528e292d7bd4b32cf3edb7cdf08ee7fda226
Lucas Di Rico <lucasdirico.ros@gmail.com>
2021-10-26 21:54:42 -0300

    Cambio de CrudRepository a JpaRepository

--- src/main/java/org/lucas/alkemy/challenge/personaje/PersonajeRepository.java
+++ src/main/java/org/lucas/alkemy/challenge/personaje/PersonajeRepository.java
@@ -3,10 +3,9 @@ package org.lucas.alkemy.challenge.personaje;
 import java.util.List;
 import java.util.Optional;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonajeRepository extends CrudRepository<Personaje, Long> {
+public interface PersonajeRepository extends JpaRepository<Personaje, Long> {
 	
 	public List<PersonajeParaLista> findAllBy();
 	
