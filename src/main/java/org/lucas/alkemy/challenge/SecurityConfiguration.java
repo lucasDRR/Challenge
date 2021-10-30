a4a5e272c383fca0f27bfc69926bc56976ae2ecf
Lucas Di Rico <lucasdirico.ros@gmail.com>
2021-10-30 02:59:09 -0300

    endpoint para login retorna jwt y método beta para registro.

--- src/main/java/org/lucas/alkemy/challenge/SecurityConfiguration.java
+++ src/main/java/org/lucas/alkemy/challenge/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.lucas.alkemy.challenge;
 import org.lucas.alkemy.challenge.user.AppUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +17,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private AppUserDetailsService appUserDetailsService;
+	
+	//@Autowired
+	//private JwtRequestFilter jwtRequestFilter;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -24,11 +28,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("auth/**").permitAll()
+		.antMatchers("/characters/**").hasAuthority("USER")
+		.antMatchers("/movies/**").hasAuthority("USER");
+		
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	
 
 }
--- src/main/java/org/lucas/alkemy/challenge/user/UserController.java
+++ src/main/java/org/lucas/alkemy/challenge/user/UserController.java
@@ -1,6 +1,14 @@
 package org.lucas.alkemy.challenge.user;
 
+import org.lucas.alkemy.challenge.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +16,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 	
+	@Autowired 
+	UserRepository userRepository;
+	
+	@Autowired
+	private AuthenticationManager authManager;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+	
+	@Autowired
+	private PasswordEncoder passworEncoder;
+	
 	@PostMapping(value = "auth/login")
-	public ResponseEntity<?> userLoginJWT(@RequestBody User user){
+	public ResponseEntity<?> userLoginJWT(@RequestBody JwtAuthRequest user) throws Exception{
+				
+		try {
+			authManager.authenticate( new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		}catch(BadCredentialsException e) {
+			
+			throw new Exception("incorrect username or password ");
+		}
 		
-		return null;
+		final UserDetails UserDetails = userDetailsService.loadUserByUsername(user.getUsername());
+		
+		final String token = jwtUtils.generarToken(UserDetails);
+		
+		return ResponseEntity.ok( new JwAuthResponse(token));
 	}
 	
 	@PostMapping(value = "auth/register")
-	
 	public ResponseEntity<?> userRegistration(@RequestBody User user){
 		
-		return null;
+		// Falta validar los datos
+		
+		user.setPassword( passworEncoder.encode(user.getPassword()));
+		
+		this.userRepository.saveAndFlush(user);
+		
+		return ResponseEntity.ok("Registración correcta");
 	}
 	
 		
